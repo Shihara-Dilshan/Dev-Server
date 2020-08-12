8672cb5ab704db5edbe18b7ee3e141a75960e92f
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-12 23:56:34 +0800

    Finished and polished

--- app.mjs
+++ app.mjs
@@ -1,18 +1,1 @@
-class Node{
-    constructor(name){
-        this.name = name;
-    }
-
-    printName = () => { 
-        console.log(this.name);
-    }
-}
-
-class Main{
-    static main = () => {
-        let obj = new Node("shihara");
-        obj.printName();
-    }
-}
-
-Main.main();
+console.log('Edit this file')
